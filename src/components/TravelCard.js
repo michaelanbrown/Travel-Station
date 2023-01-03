05be203c8085db9deff1fee8f89a2fce4d95efe5
Michaela Brown <m_brownbrown@yahoo.com>
2023-01-02 17:58:22 -0800

    styling changes

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -5,7 +5,7 @@ function TravelCard({ travel }) {
     
     return (
         <div className='TravelCard'>
-            <img  src={travel.photo} alt={travel.city} width="18%" height="18%"/>
+            <img  src={travel.photo} alt={travel.city} width="75%" height="75%"/>
             <p>{travel.city}<br></br>{travel.state}<br></br>{travel.date}</p>
             <br></br>
             <br></br>
