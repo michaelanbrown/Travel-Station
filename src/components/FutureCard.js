fca1a802cb6a5f7294adfcffff01ad1c7e38e952
Michaela Brown <m_brownbrown@yahoo.com>
2023-01-03 19:24:00 -0800

    future plans styling

--- src/components/FutureCard.js
+++ src/components/FutureCard.js
@@ -5,7 +5,10 @@ function FutureCard({ futureEvent }) {
     
     return (
             <div className="Future">
+                <br></br>
+                <img className="FuturePhoto" src={futureEvent.photo} alt={futureEvent.city} width="75%" height="75%" />
                 <p>{futureEvent.city}<br></br>{futureEvent.state}<br></br>{futureEvent.date}<br></br>{futureEvent.reason}</p>
+                <br></br>
                 <br></br>
                 <br></br>
             </div>
